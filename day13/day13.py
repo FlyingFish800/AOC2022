8e6cb282813810f8bb76f452967a82ba7d8286f9
alex <aesymons888@gmail.com>
2022-12-13 22:30:19 -0800

    Validated and documented days 13 and 14

--- day13/day13.py
+++ day13/day13.py
@@ -1,27 +1,44 @@
+# Load data line by line
 data = [line.strip('\n') for line in open("input.txt", 'r').readlines()]
 
+# Make x a list if it isnt
 to_list = lambda x : x if type(x) == list else [x]
 
+# Compare two lists recursively and retrun true if theyre in right order
 def compare(left, right, base_case = False):
+    # If both are ints compare as ints. None means keep going
     if isinstance(left, int) and isinstance(right, int):
         if left == right: return None
         return  left < right
 
+    # Make both sides lists
     left = to_list(left)
     right = to_list(right)
 
+    # If left is emtpy, they are in right order
     if len(left) == 0: return True
+
+    # For all positions in left list
     for i in range(len(left)):
+        # If its out of scope of right list, they are not in the right order
         if i >= len(right): return False
+
+        # If both are empty, keep checking
         if left[i] == [] and right[i] == []: continue
 
+        # Compare recursively the items at this index. If its None, continue
         result = compare(left[i],right[i])
         if not result == None:
             return result
+    
+    # If all items were fine up until this point, they are in the right order if right list is longer,
+    # or if weve reached the end
     return True if base_case or len(left) < len(right) else None
 
+# List of indexes in the rifht order
 in_right_order = []
 
+# Use eval to parse each side, then compare them. Add to list if theyre in the right order. Sum for answer
 for pair in range(0,len(data),3):
     left_side = eval(data[pair])
     right_side = eval(data[pair+1])
--- day14/day14.py
+++ day14/day14.py
@@ -1,20 +1,28 @@
+# Load file so each set of points is on its own line
 data = [line.strip('\n') for line in open("input.txt", 'r').readlines()]
 
-
 screen = []
-
 maxy = 0
+
+# Reset screen, draw lines, set max y value
 def reset():
     global screen, maxy
+    # Screen is [200][1000] [y][x]
     screen = [['.' for i in range(1000)] for i in range (200)]
     screen[0][500] = '+'
+    maxy = 0
 
+    # Load in every shape. Line as in line of file
     for line in data:
+        # Reset last point, and break all points int [(x1,y1), ...]
         last = None
         points = [point.split(',') for point in line.split(' ') if not point == '->']
+
         for point in points:
             px = int(point[0])
             py = int(point[1])
+
+            # Update max y value if necessary, and set last if this is first point
             if py > maxy: maxy = py
             if last == None:
                 last = (px,py)
@@ -23,18 +31,21 @@ def reset():
             lx = last[0]
             ly = last[1]
 
+            # Draw a line for all x,y points between the two points, set points to '#'
             for x in range(min(lx,px), max(lx+1,px+1)):
                 for y in range(min(ly,py),max(ly+1,py+1)):
                     screen[y][x] = '#'
 
             last = (px,py)
 
+# Print for debugging. Szie fits in terminal and shows important stuff
 def print_screen():
     for y in range(0,200):
         for x in range(450,550):
             print(screen[y][x],end='')
         print()
 
+# Determine move if one can be made. None: fallen off, 1: down, 2: down/left, 3: down/right in that priority order
 def can_move(x,y):
     try:
         if screen[y+1][x] == '.':
@@ -47,19 +58,31 @@ def can_move(x,y):
     except IndexError:
         return None
 
+# Simulate one grain of sand in the furrent enviroment. Returns wether or not another grain can be simulated
 def simulate_sand():
+    # Start at origin, and determine initial move
     x = 500
     y = 0
     move = can_move(x,y)
+
+    # While move works
     while move > 0:
-        y += 1
-        if move == 2: x -= 1
+        y += 1 # All moves go down 1
+        if move == 2: x -= 1 # Move horixontally as perscribed
         if move == 3: x += 1
+
+        # Determine next move, return False to stop more sand if one fell into void (return False)
         move = can_move(x,y)
         if move == None: return False
+
+    # If no moves were taken, return False to stop simulation and dont overwrite origin '+'
+    if x == 500 and y == 0: return False
+
+    # Set potition to have grain of sand and keep simulating more
     screen[y][x] = 'o'
     return True
 
+# For part 1: Reset screen, simulate sand while they arent falling into void, and print tally
 reset()
 #print_screen()
 
@@ -70,15 +93,17 @@ while simulate_sand():
 
 print("Part1:", sands)
 
+# For part 2: Reset and use max y to add floor, then run as before adding 1 for the origin
 reset()
-print(maxy)
+#print(maxy)
+
 for x in range(0,1000):
-    screen[maxy][x] = '#'
+    screen[maxy+2][x] = '#'
 
 sands = 0
 while simulate_sand():
     #print_screen()
     sands += 1
 
-print_screen()
-print("Part2:", sands)
+#print_screen()
+print("Part2:", sands+1)
